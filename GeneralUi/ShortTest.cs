5b8890b3ba9360d996ace53e16dd3e8dabb99e00
Brian C. Miller <brian.c.miller@frontier.com>
2015-11-13 17:14:17 -0800

    Added command line tests and more unit tests.

--- GeneralUi/ShortTest.cs
+++ GeneralUi/ShortTest.cs
@@ -15,18 +15,34 @@ namespace GeneralUi
     public class ShortTest
     {
         /// <summary> Active API key to access the Google API. </summary>
-        static string apiKey = "";
+        static string apiKey = System.Environment.GetEnvironmentVariable("GoogleShortenerApiKey");
 
         /// <summary>
         /// This unit test tests the basic happy path for the GetShortUri method.
         /// </summary>
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("http://www.google.com/")]
+        [InlineData("https://www.google.com/")]
+        [InlineData(@"https://en.wiktionary.org/wiki/%E7%93%9C")]   // These two show as Japanese Kanji in the browser.
+        [InlineData(@"http://www.docoja.com:8080/kanji/keykanj?dbname=kanjig&keyword=%E3%81%86%E3%82%8A")]
+        public void HappyPath(string testUri)
         {
-            string testUri = "http://www.google.com/";
             string shortUri = ShortenUri.GetShortUri(testUri, apiKey);
             Assert.True(string.IsNullOrEmpty(shortUri) == false, "shortUri");
             Assert.Equal(testUri, this.GetResponseUri(shortUri));
+        }
+
+        [Theory]
+        [InlineData("1234.123")]
+        [InlineData("1234 123")]
+        [InlineData("this is not a pipe nor a good uri")]
+        [InlineData(" ")]
+        [InlineData("\t")]
+        [InlineData("\\")]
+        public void BadUrlFormat(string testUri)
+        {
+            string shortUri = string.Empty;
+            Assert.Throws(typeof(System.Net.WebException), () => shortUri = ShortenUri.GetShortUri(testUri, apiKey));
         }
 
         /// <summary>
--- README.md
+++ README.md
@@ -9,3 +9,15 @@ The projects in this solution are the ShortenUri class, and the GeneralUi
 test project.  The test framework used is XUnit, using the Visual Studio
 test integration.
 
+The packages directory in the solution root was not added, since it contains
+16Mb of compiled code for XUnit.  Follow the instructions on the XUnit wiki
+to add it to the project.
+
+##Running the Command Line Tests
+
+The tools directory contains the XUnit command line utility. Use it as follows:
+
+    set GoogleShortenerApiKey=__Some Value Here for API key__
+    tools\xunit.console\xunit.console.exe GeneralUi\bin\debug\GeneralUi.dll
+
+The test results will be printed out on the command line.
